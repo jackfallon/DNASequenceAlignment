eba8c76cb129593d6212c1ce92abe46d9826b541
jackfallon <77027010+jackfallon@users.noreply.github.com>
2024-08-01 03:54:34 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <SFML/System.hpp>
+#include "EDistance.hpp"
+
+
+int main() {
+    sf::Clock clock;
+    sf::Time t;
+
+    std::string s1;
+    std::string s2;
+
+    std::cin >> s1;
+    std::cin >> s2;
+    EDistance e1(s1, s2);
+    int distance = e1.optDistance();
+    std::cout << "Edit distance = " << distance << std::endl;
+    std::string align = e1.alignment();
+    std::cout << align << std::endl;
+    t = clock.getElapsedTime();
+    std::cout << "Execution time is " << t.asSeconds() << " seconds" << std::endl;
+    return 0;
+}
